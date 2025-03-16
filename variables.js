bc9fcbf1be95c3311f99a753a9c332e995f39e99
Hari21-Tech <sri21harisesh@gmail.com>
2025-03-16 16:43:33 +0530

    new files

---
+++ variables.js
@@ -0,0 +1,11 @@
+const accountId = 100 // constant define kara hai
+let accountEmail = "Hari"
+var accountPAssword = "1234"
+accountCity = "P:une"
+let hello // by default set to "undefined".
+
+// accountId = 200 // error dega kyuki constant hai
+
+console.table([accountId, accountEmail, accountPAssword, accountCity, hello])
+
+
