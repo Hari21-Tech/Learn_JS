824e13083c92c7c575f8e89aacecd13d6fa86caf
Hari21-Tech <sri21harisesh@gmail.com>
2025-03-16 17:19:12 +0530

    New new

--- datatypes.js
+++ datatypes.js
@@ -2,3 +2,16 @@
 
 // alert(3+3) // we are using nodejs, not browser
 
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
